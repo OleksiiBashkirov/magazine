2b9427315b961f4ae45ab1ba6b73918f3dc57ac6
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-29 07:59:05 +0300

    - создано класс Product

---
+++ src/main/java/bashkirov/magazine/model/Product.java
@@ -0,0 +1,37 @@
+package bashkirov.magazine.model;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private int id;
+
+    @NotBlank(message = "Cannot be empty")
+    @Size(min = 1, max = 64, message = "Cannot be less 1 and more 64 characters")
+    private String name;
+
+    @NotBlank
+    @Size(min = 9, max = 9, message = "Must be exactly 9 symbols")
+    @Pattern(regexp = "^\\d{4}-\\d{4}", message = "Must be like `1234-1234`")
+    private String article;
+
+    @Min(value = 0, message = "Cannot be less 0")
+    @Max(value = 100_000, message = "Cannot be more 100 000")
+    private double price;
+
+    @Min(value = 0, message = "Cannot be less 0")
+    @Max(value = 10_000, message = "Cannot be more 10 000")
+    private double quantity;
+
+    @Size(max = 256, message = "Cannot be more 256 characters")
+    private String description;
+}
