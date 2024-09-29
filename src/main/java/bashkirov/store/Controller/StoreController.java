ef8a7d88dec5d40fc996236658614b94df4c2d36
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-29 18:01:10 +0300

    - Створено StoreController.java з методом getById - створено store-new-page.html

---
+++ src/main/java/bashkirov/store/Controller/StoreController.java
@@ -0,0 +1,28 @@
+package bashkirov.store.Controller;
+
+import bashkirov.store.dao.StoreDao;
+import bashkirov.store.validation.StoreValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/store")
+@RequiredArgsConstructor
+public class StoreController {
+    private final StoreDao storeDao;
+    private final StoreValidator storeValidator;
+
+    @GetMapping("/{id}")
+    public String getById(
+            @PathVariable("id") int storeId,
+            Model model
+    ) {
+        model.addAttribute("storeGetById", storeDao.getById(storeId));
+        return "store-new-page";
+    }
+
+}
