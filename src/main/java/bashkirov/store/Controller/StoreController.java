85691057c6652d7a3c751f3d871e9718d314feef
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-30 05:15:03 +0300

    - StoreController.java: додано методи storeNewPage та save
    - stores-page.html: додано кнопку Add new Store
    
    - створено store-new-page.html з можливістю зберігти новий Store
    
    - checkstyle зміни в Store.java та store-page.html
    - незначні зміни в StoreValidator.java

--- src/main/java/bashkirov/store/Controller/StoreController.java
+++ src/main/java/bashkirov/store/Controller/StoreController.java
@@ -1,12 +1,17 @@
 package bashkirov.store.Controller;
 
 import bashkirov.store.dao.StoreDao;
+import bashkirov.store.model.Store;
 import bashkirov.store.validation.StoreValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,7 +27,7 @@ public class StoreController {
             Model model
     ) {
         model.addAttribute("storeGetById", storeDao.getById(storeId));
-        return "store-new-page";
+        return "store-page";
     }
 
     @GetMapping()
@@ -31,6 +36,26 @@ public class StoreController {
     ) {
         model.addAttribute("storeList", storeDao.getAll());
         return "stores-page";
+    }
+
+    @GetMapping("/new")
+    public String storeNewPage(
+            @ModelAttribute("newStore") Store newStore
+    ) {
+        return "store-new-page";
+    }
+
+    @PostMapping()
+    public String save(
+            @Valid @ModelAttribute("newStore") Store newStore,
+            BindingResult bindingResult
+    ) {
+        storeValidator.validate(newStore, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "store-new-page";
+        }
+        storeDao.save(newStore);
+        return "redirect:/store";
     }
 
 
