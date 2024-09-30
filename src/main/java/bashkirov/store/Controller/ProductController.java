3198fbf6c9d7a4ddebccadca872bfefa3f0843c0
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-30 06:38:43 +0300

    - створено ProductController.java з методом getById - створено product-page.html з інформацією про продукт

---
+++ src/main/java/bashkirov/store/Controller/ProductController.java
@@ -0,0 +1,26 @@
+package bashkirov.store.Controller;
+
+import bashkirov.store.dao.ProductDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductDao productDao;
+
+    @GetMapping("/{id}")
+    public String getById(
+            @PathVariable("id") int productId,
+            Model model
+    ) {
+        model.addAttribute("productGetById", productDao.getById(productId));
+        return "product-page";
+    }
+
+}
