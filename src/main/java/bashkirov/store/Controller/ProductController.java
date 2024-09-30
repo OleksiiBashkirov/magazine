42c971f967b33e468a3116bdef0bead902b4fcc4
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-30 08:43:02 +0300

    - ProductController.java: додано метод delete - product-page.html: додано кнопку Delete

--- src/main/java/bashkirov/store/Controller/ProductController.java
+++ src/main/java/bashkirov/store/Controller/ProductController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -83,4 +84,11 @@ public class ProductController {
         return "redirect:/product/" + productId;
     }
 
+    @DeleteMapping("/{id}")
+    public String delete(
+            @PathVariable("id") int productId
+    ) {
+        productDao.delete(productId);
+        return "redirect:/product";
+    }
 }
